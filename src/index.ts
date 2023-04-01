05a98a0c26a4bc4a96a79636eb49799bada09225
julielaursen <julielaursen1@gmail.com>
2023-04-01 16:10:27 -0500

    add google map example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+/// <reference types="@types/google.maps" />
+import { User } from './User';
+import { Company } from './Company';
+const user = new User();
+console.log(user);
+const company = new Company();
+console.log(company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+};
